1c9b81e6413254e35fb066f27ee9f11ada32c473
Nikolay Eckert <nikolay-eckert@yandex.ru>
2020-12-01 13:36:10 +0700

    Remove the old code

--- Shop/Common/Classes/Shop/ShopType.swift
+++ Shop/Common/Classes/Shop/ShopType.swift
@@ -8,14 +8,6 @@
 import Foundation
 
 enum ShopType: Int, CaseIterable, Codable {
-//	func encode(to encoder: Encoder) throws {
-//
-//	}
-//
-//	init(from decoder: Decoder) throws {
-//		try self.init(from: decoder)
-//	}
-//
 	private enum CodingKeys: String, CodingKey {
 		case department
 		case grocery
