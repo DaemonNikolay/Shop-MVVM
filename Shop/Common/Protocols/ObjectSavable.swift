dc25f15bbd20ed0bc8d5cad7ab6fab52227a5f2b
Nikolay Eckert <nikolay-eckert@yandex.ru>
2020-12-01 13:36:42 +0700

    Update comments

--- Shop/Common/Protocols/ObjectSavable.swift
+++ Shop/Common/Protocols/ObjectSavable.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 
+/// https://medium.com/flawless-app-stories/save-custom-objects-into-userdefaults-using-codable-in-swift-5-1-protocol-oriented-approach-ae36175180d8
 protocol ObjectSavable {
 	func setObject<Object>(_ object: Object, forKey: String) throws where Object: Encodable
 	func getObject<Object>(forKey: String, castTo type: Object.Type) throws -> Object where Object: Decodable
