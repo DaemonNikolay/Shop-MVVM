b0a9538ebb2814e678f901dd537f8ae6f51bc97a
Aleksandr Boriskin <aboriskin@65apps.com>
2020-12-03 11:45:04 +0700

    минорные изменения

--- Shop/Scenes/ShopList/View/ShopListViewController.swift
+++ Shop/Scenes/ShopList/View/ShopListViewController.swift
@@ -17,11 +17,11 @@ class ShopListViewController: UIViewController, UITableViewDelegate, UITableView
 	
 	init (container: Container) {
 		self.viewModel = container.viewModel
-		super.init(nibName: "ShopListViewController", bundle: nil)
+		super.init(nibName: nil, bundle: nil)
 	}
 	
 	required init?(coder aDecoder: NSCoder) {
-		fatalError("")
+		fatalError("unable to use init with coder")
 	}
 	
 	override func viewDidLoad() {
