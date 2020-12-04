d2c81415412ebbec7592333238062a4dab57146a
Nikolay Eckert <nikolay-eckert@yandex.ru>
2020-12-04 14:52:19 +0700

    Оптимизация

--- Shop/Common/Classes/Shop/OfficeHours.swift
+++ Shop/Common/Classes/Shop/OfficeHours.swift
@@ -9,42 +9,34 @@ import Foundation
 import os.log
 
 struct OfficeHours: Codable {
-	private var _opening: Float = .zero
-	var opening: Float {
-		set {
-			let value = abs(newValue)
-			
-			if value > 24 {
+	private let timeLimit: Float = 24.59
+	
+	var opening: Float = .zero {
+		didSet {
+			let value = abs(opening)
+
+			if value > timeLimit {
 				os_log("The opening time must be in the range from 0 to 24 and more closing time.",
 					   log: .default,
 					   type: .fault)
-				
-				return
+
+				opening = oldValue
 			}
-			
-			self._opening = value
 		}
-		
-		get { self._opening }
 	}
 	
-	private var _closing: Float = .zero
-	var closing: Float {
-		set {
-			let value = abs(newValue)
+	var closing: Float = .zero {
+		didSet {
+			let value = abs(closing)
 			
-			if value > 24 {
+			if value > timeLimit {
 				os_log("The closing time must be in the range from 0 to 24 and more opening time.",
 					   log: .default,
 					   type: .fault)
 				
-				return
+				closing = oldValue
 			}
-			
-			self._closing = value
 		}
-		
-		get { self._closing }
 	}
 	
 	init(opening: Float, closing: Float) {
