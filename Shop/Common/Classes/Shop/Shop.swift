807446bdeeac6e8a21d04e27be3db4e2d5ffd68c
Nikolay Eckert <nekkert@65apps.com>
2020-12-07 12:05:13 +0700

    Удалены лишних методов для Decode&Encode

--- Shop/Common/Classes/Shop/Shop.swift
+++ Shop/Common/Classes/Shop/Shop.swift
@@ -32,40 +32,6 @@ class Shop: Codable {
 		self.officeHours = officeHours
 		self.isNearestShop = isNearestShop
 	}
-	
-	private enum CodingKeys: String, CodingKey {
-		case id
-		case type
-		case employeesNumber
-		case openingDate
-		case name
-		case officeHours
-		case isNearestShop
-	}
-	
-	required init(from decoder: Decoder) throws {
-		let values = try decoder.container(keyedBy: CodingKeys.self)
-		
-		self.id = try values.decode(UUID.self, forKey: .id)
-		self.type = try values.decode(ShopType.self, forKey: .type)
-		self.employeesNumber = try values.decode(UInt.self, forKey: .employeesNumber)
-		self.openingDate = try values.decode(Date.self, forKey: .openingDate)
-		self.name = try values.decode(String.self, forKey: .name)
-		self.officeHours = try values.decode(OfficeHours.self, forKey: .officeHours)
-		self.isNearestShop = try values.decode(Bool.self, forKey: .isNearestShop)
-	}
-	
-	func encode(to encoder: Encoder) throws {
-		var container = encoder.container(keyedBy: CodingKeys.self)
-		
-		try container.encode(self.id, forKey: .id)
-		try container.encode(self.type, forKey: .type)
-		try container.encode(self.employeesNumber, forKey: .employeesNumber)
-		try container.encode(self.openingDate, forKey: .openingDate)
-		try container.encode(self.name, forKey: .name)
-		try container.encode(self.officeHours, forKey: .officeHours)
-		try container.encode(self.isNearestShop, forKey: .isNearestShop)
-	}
 }
 
 extension Shop {
